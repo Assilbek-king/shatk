0bbfe79503458c28c2cc5c64a74e68f1b09b6a02
Assilbek <72435565+Assilbek-king@users.noreply.github.com>
2026-03-18 12:49:44 +0500

    Refactor URL imports and update urlpatternsurl

--- shymkent-master/college/urls.py
+++ shymkent-master/college/urls.py
@@ -1,35 +1,22 @@
-"""college URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls import url, include
+from django.urls import path, re_path
 from django.views.static import serve
 from college import settings
-from main.views import indexHandler, NewsDetailHandler, SpecialtyDetailHandler, CourseHandler,  TeacherHandler
-from main.views import AboutHandler, BazaHandler, VideoHandler, QabyldauHandler, BiliktilikHandler, LicenseHandler
-from main.views import AcredatsiyaHandler, QurylymHandler, OquAdistemeHandler, AdistemelikKabinetHandler
-from main.views import JasMamanHandler, BirlestikHandler, JetistikHandler, OquHandler, OquUrdisiHandler, KesteHandler
-from main.views import StudentHandler, AqparatHandler, JumysqaOrnalasuHandler,  PartnerHandler, SaualnamaHandler
-from main.views import MissiyaHandler, JemqorlyqHandler, KenesJosparyHandler, StudenttikKenesHandler, BitirushilerHandler
-from main.views import StudenttikOmirHandler, TalapkerHandler, OquHandler, TulekterHandler
+
+from main.views import (
+    indexHandler, NewsDetailHandler, SpecialtyDetailHandler, CourseHandler, TeacherHandler,
+    AboutHandler, BazaHandler, VideoHandler, QabyldauHandler, BiliktilikHandler, LicenseHandler,
+    AcredatsiyaHandler, QurylymHandler, OquAdistemeHandler, AdistemelikKabinetHandler,
+    JasMamanHandler, BirlestikHandler, JetistikHandler, OquHandler, OquUrdisiHandler, KesteHandler,
+    StudentHandler, AqparatHandler, JumysqaOrnalasuHandler, PartnerHandler, SaualnamaHandler,
+    MissiyaHandler, JemqorlyqHandler, KenesJosparyHandler, StudenttikKenesHandler, BitirushilerHandler,
+    StudenttikOmirHandler, TalapkerHandler, TulekterHandler
+)
 
 
 urlpatterns = [
-
     path('admin/', admin.site.urls),
+
     path('news/<int:news_id>/', NewsDetailHandler),
     path('specialty/<int:specialty_id>/', SpecialtyDetailHandler),
     path('courses/', CourseHandler),
@@ -63,11 +50,10 @@ urlpatterns = [
     path('talapker/', TalapkerHandler),
     path('tulekter/', TulekterHandler),
 
-
-    url(r'^media/(?P<path>.*)$', serve, {
+    # ✅ исправлено (url → re_path)
+    re_path(r'^media/(?P<path>.*)$', serve, {
         'document_root': settings.MEDIA_ROOT
     }),
-
 
     path('', indexHandler),
 ]
